7865d7e54fe27cbc0fe601be1e1a888b41cdf641
Rick Hartley <grickyh@gmail.com>
2023-03-29 00:47:00 +0700

    audio functionality and new layout and elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import logo from './logo.png';
 import Navbar from './Navbar';
+import sound from './Chapter1ofmastery.mp3';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -9,6 +10,12 @@ function App() {
   const [summary, setSummary] = useState({ text: '', tips: [] });
   const [chapters, setChapters] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [bookTitle, setBookTitle] = useState('');
+  const [bookAuthor, setBookAuthor] = useState('');
+  // const [isPlaying, setIsPlaying] = useState(false);
+  const [audioFile, setAudioFile] = useState('');
+
+
   const chaptersPerPage = 7;
 
   useEffect(() => {
@@ -31,48 +38,54 @@ function App() {
   };
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    const response = await fetch('http://localhost:3001', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ message })
-    });
-    const data = await response.json();
-    const text = data.choices[0].text.trim();
-    console.log(text);
-    setResponse(text);
-    setMessage('');
-  };
-
-  // const handleChapterClick = async (chapter) => {
-  //   const response = await fetch('http://localhost:3001', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({
-  //       message: `Summarize chapter "${chapter}" in 100 words and provide 3 practical life tips and an ending message message after the tips saying "I hope you are able to apply these tips in your own life provided by Chapterly.tech"`
-  //     }),
-  //   });
-  //   const data = await response.json();
-  //   const text = data.choices[0].text.trim();
+  event.preventDefault();
+  const response = await fetch('http://localhost:3001', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ message })
+  });
+  const data = await response.json();
+  const text = data.choices[0].text.trim();
+  console.log(text);
+  setResponse(text);
+  const title = text.match(/Title: (.*)Author:/)[1].trim();
+  const author = text.match(/Author: (.*)Chapter 1:/)[1].trim();
+  setBookTitle(title);
+  setBookAuthor(author);
+  setMessage('');
+};
   
-  //   // Parse text into summary and tips
-  //   const summaryStartIndex = text.indexOf('Chapter');
-  //   const summaryEndIndex = text.indexOf('Three practical life tips') || ('practical life tips');
-  //   const summary = text.substring(summaryStartIndex, summaryEndIndex).trim();
-  //   const tipsStartIndex = text.indexOf('Three practical life tips') || ('practical life tips');
-  //   // const tipsEndIndex = text.indexOf('1)') - 1;
-  //   const tipsEndIndex = text.indexOf('.tech');
-  //   const tipsText = text.substring(tipsStartIndex, tipsEndIndex).trim();
-  //   const tips = tipsText.split('\n').filter(tip => tip.trim() !== '');
-  //   console.log(text);
-  //   console.log('Summary:', summary);
-  //   console.log('Tips:', tips);
-  //   setSummary({ summary: summary, tips: tips });
-  // };
+// const handleSubmit = async (event) => {
+//   event.preventDefault();
+//   const response = await fetch('http://localhost:3001', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ message })
+//   });
+//   const data = await response.json();
+//   const text = data.choices[0].text.trim();
+//   console.log(text);
+//   setResponse(text);
+//   setBookTitle(data.title);
+//   setBookAuthor(data.author);
+//   setMessage('');
+
+//   // fetch book cover image URL
+//   const bookTitleFormatted = data.title.replace(/ /g, '+');
+//   const bookAuthorFormatted = data.author.replace(/ /g, '+');
+//   const bookCoverResponse = await fetch(`https://bookcover-api.onrender.com/bookcover?book_title=${bookTitleFormatted}&author_name=${bookAuthorFormatted}`);
+//   const bookCoverData = await bookCoverResponse.json();
+//   if (bookCoverData.error) {
+//     setBookCover(null);
+//   } else {
+//     setBookCover(bookCoverData.cover_url);
+//   }
+// };
+
 
   const handleChapterClick = async (chapter) => {
     const response = await fetch('http://localhost:3001', {
@@ -87,15 +100,25 @@ function App() {
     const data = await response.json();
     const text = data.choices[0].text.trim();
     console.log(text);
-    setSummary({ text }); // Update this line to parse the text into summary and tips.
+    setSummary({ text });
+    setAudioFile({ sound });
+    console.log(audioFile); // add this line
+
   };
-  
-  
-  
-  
-  
-  
-  
+
+  //audio 
+// eslint-disable-next-line
+  const audio = useRef(null);
+
+// const playAudio = () => {
+//   audio.current.play();
+//   setIsPlaying(true);
+// };
+
+// const pauseAudio = () => {
+//   audio.current.pause();
+//   setIsPlaying(false);
+// };
 
   const handleChange = (event) => {
     setMessage(event.target.value);
@@ -110,15 +133,25 @@ function App() {
           <form className="message-input-form" onSubmit={handleSubmit}>
             <label className="search-label">
               Please Enter A Book Title
-              <input type="text" value={message} onChange={handleChange} />
+              <input className= "input" type="text" value={message} onChange={handleChange} />
             </label>
-            <button type="submit">Send</button>
+            <button className="search" type="submit">Send</button>
           </form>
+          {/* <audio ref={audio} controls>
+  <source src={ sound } type="audio/mp3" />
+</audio> */}
         </div>
         <img src={logo} className="logo" alt="logo" />
         <div className="response">
-  {response && (
-    <div>
+          {response && (
+            <div>
+              <div className="book-info">
+                <h2>Title: {bookTitle}</h2>
+                <h4>Author: {bookAuthor}</h4>
+                {/* {bookCover && (
+    <img src={bookCover} alt={`Cover of ${bookTitle}`} className="book-cover" />
+  )} */}
+              </div>
       <div className="outlined-box">
         {chapters
           .slice(
@@ -139,14 +172,18 @@ function App() {
         Close
       </button>
       {summary && (
-        <div className="summary-overlay">
-          <div className="summary-container">
-            <h3>Chapterized:</h3>
-            <p>{summary.text}</p>
-            <button className="close-button" onClick={() => setSummary(null)}>Close</button>
-          </div>
-        </div>
-      )}
+  <div className="summary-overlay">
+    <div className="summary-container">
+      <h3>Chapterized:</h3>
+      <p>{summary.text}</p>
+      <audio ref={audio} controls>
+  <source src={ sound } type="audio/mp3" />
+</audio>
+      <button className="close-button" onClick={() => setSummary(null)}>Close</button>
+    </div>
+  </div>
+)}
+
     </div>
   )}
 </div>
